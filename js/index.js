462e20d4e40a33fb1c1885b1993db3fa361066a9
Toño <german.velandia@swissaxis.ch>
2023-01-17 16:48:22 -0500

    toush

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //(function () {
-    const $this = this;
+    const $DOM = this;
 
     Item = function (name, details, action) {
         this.name = name;
@@ -79,14 +79,14 @@
     ];
 
     const ctx = {
-        pantalla: $this.pantalla.getContext("2d"),
-        mapa: $this.mapa.getContext("2d"),
-        fondo: $this.fondo.getContext("2d"),
-        juego: $this.canvas.getContext("2d"),
+        pantalla: $DOM.e_pantalla.getContext("2d"),
+        mapa: $DOM.e_mapa.getContext("2d"),
+        fondo: $DOM.e_fondo.getContext("2d"),
+        juego: $DOM.e_canvas.getContext("2d"),
     }
 
-    const w = $this.canvas.width;
-    const h = $this.canvas.height;
+    const w = $DOM.e_canvas.width;
+    const h = $DOM.e_canvas.height;
 
     var keydown = []
 
@@ -94,6 +94,18 @@
         frameCount: 0,
         currentFps: 0,
         lasFrame: new Date().getTime(),
+        show () {
+            $DOM.e_fps.innerHTML = "FPS: " + this.currentFps
+        },
+        run () {
+            let actualFrame = new Date().getTime();
+            if (actualFrame - this.lasFrame >= 1000) {
+                this.currentFps = this.frameCount;
+                this.frameCount = 0;
+                this.lasFrame = actualFrame;
+                this.show()
+            } this.frameCount++;
+        }
     }
 
     const img = function(src){
@@ -128,11 +140,11 @@
         {
             bloque: 0, // b
             vida: 3, // v
-            name: $this.name, // n
+            name: $DOM.e_name.value, // n
             item: -1, // o
             id: null, // i
             vuelta: 0, // m
-            color: $this.color.value,
+            color: $DOM.e_color.value,
             preso: false, // p = 0
             x: 0,
             y: 0,
@@ -159,7 +171,7 @@
     ]
 
     Bloque.prototype = {
-        tocar: function (x, y) {
+        isTouching: function (x, y) {
             if (
                 x < (this.x * 20) / div ||
                 x > (this.x * 20) / div + (this.w * 20) / div ||
@@ -168,6 +180,8 @@
             ) return false;
             return true;
         },
+
+
     }
 
     const blockList = [
@@ -379,60 +393,77 @@
         new Bloque((w / 20) * 0, (h / 20) * 1, w / 20, h / 20, 0),
     ]
 
-    BtnController = function (x, y, w, h, img) {
+    BtnController = function (x, y, w, h, img, fun = () => false ) {
         this.h = Math.round(h);
         this.w = Math.round(w);
         this.x = Math.round(x);
         this.y = Math.round(y);
         this.img = img;
+        this.run = fun
     }
 
     BtnController.prototype = {
-        containsPoint: function (x, y) {
-          return ( x < this.x || x > this.x + this.width || y < this.y || y > this.y + this.width) ? false : true;
+        isTouching (x, y) {
+            return ( x < this.x || x > this.x + this.w || y < this.y || y > this.y + this.w) ? false : true
         },
     }
 
     const getBtnList = function(){
         return [
             new BtnController(
-                $this.pantalla.width * 0.08 - $this.pantalla.height / 9,
-                $this.pantalla.height / 2 + $this.pantalla.height / 9 - $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
+                $DOM.e_pantalla.width * 0.08 - $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 2 + $DOM.e_pantalla.height / 9 - $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
                 imageList.btnLeft
             ),
             new BtnController(
-                $this.pantalla.width * 0.08 + $this.pantalla.height / 9,
-                $this.pantalla.height / 2 + $this.pantalla.height / 9 - $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
+                $DOM.e_pantalla.width * 0.08 + $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 2 + $DOM.e_pantalla.height / 9 - $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
                 imageList.btnRight
             ),
             new BtnController(
-                $this.pantalla.width * 0.67,
-                $this.pantalla.height / 2 + $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
+                $DOM.e_pantalla.width * 0.67,
+                $DOM.e_pantalla.height / 2 + $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
                 imageList.btnDown
             ),
             new BtnController(
-                $this.pantalla.width * 0.67,
-                $this.pantalla.height / 2 + $this.pantalla.height / 9 - ($this.pantalla.height / 9) * 2,
-                $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
+                $DOM.e_pantalla.width * 0.67,
+                $DOM.e_pantalla.height / 2 + $DOM.e_pantalla.height / 9 - ($DOM.e_pantalla.height / 9) * 2,
+                $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
                 imageList.btnUp
             ),
             new BtnController(
-                $this.pantalla.width * 0.67 - ($this.pantalla.height / 9) * 2,
-                $this.pantalla.height / 2 + $this.pantalla.height / 9 - $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
-                $this.pantalla.height / 9,
+                $DOM.e_pantalla.width * 0.67 - ($DOM.e_pantalla.height / 9) * 2,
+                $DOM.e_pantalla.height / 2 + $DOM.e_pantalla.height / 9 - $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
+                $DOM.e_pantalla.height / 9,
                 imageList.run
             ),
         ]
     }
+    var teclas = {
+        87: () => {
+            up()
+        },
 
+        83: () => {
+            down
+        },
+
+        65: () => {
+            left
+        },
+
+        68: () => {
+            right
+        }
+    }
     const controller = {
         launchFullScreen: function(element) {
             if (element.requestFullScreen) {
@@ -454,20 +485,41 @@
             }
         },
 
+        keyTestButton: function(){
+            
+        },
+
         useKeyboard: function() {
             window.addEventListener( "keydown", function (e) {
-                keydown[e.keyCode] = true;
+                keydown[e.keyCode] = true
+                this.keyTestButton()
             }, false );
 
             window.addEventListener( "keyup", function (e) {
-                keydown[e.keyCode] = false;
+                keydown[e.keyCode] = false
+                this.keyTestButton()
             }, false );
         },
 
+        touchTestButtons: function(event){
+            event.preventDefault();
+            let target_touches = event.targetTouches
+            btnList.forEach(button => {
+                for (let index = 0; index < target_touches.length; index++) {
+                    const touch = target_touches[index];
+                    const x = (touch.clientX - $DOM.e_pantalla.getBoundingClientRect().left)
+                    const y = (touch.clientY - $DOM.e_pantalla.getBoundingClientRect().top)
+                    if (button.isTouching(x, y)) console.log(button.run())
+                    //console.log(x , y, button.x, button.y)
+                }
+            })
+            console.log(target_touches)
+        },
+
         useTouch: function(){
-            $this.pantalla.addEventListener("touchend", game.touchEnd, { passive: false });
-            $this.pantalla.addEventListener("touchmove", game.touchMove, { passive: false });
-            $this.pantalla.addEventListener("touchstart", game.touchStart, { passive: false,});
+            $DOM.e_pantalla.addEventListener("touchend", this.touchTestButtons, { passive: false });
+            $DOM.e_pantalla.addEventListener("touchmove", this.touchTestButtons);
+            $DOM.e_pantalla.addEventListener("touchstart", this.touchTestButtons);
             window.addEventListener("resize", render.touchController);
             render.touchController()
         },
@@ -477,6 +529,13 @@
                 this.useTouch()
             else 
                 this.useKeyboard()
+        },
+
+        run () {
+            this.up();
+            this.down();
+            this.left();
+            this.right();
         }
     }
 
@@ -488,18 +547,19 @@
     }
 
     const render = {
-        touchControlle () {
-            $this.pantalla.width = $this.pantalla.width
-            $this.pantalla.width = document.documentElement.clientWidth * 1.3;
-            $this.pantalla.height = document.documentElement.clientHeight * 1.3;
+        touchController: function () {
+            $DOM.e_pantalla.width = $DOM.e_pantalla.width
+            $DOM.e_pantalla.width = document.documentElement.clientWidth * 1.3;
+            $DOM.e_pantalla.height = document.documentElement.clientHeight * 1.3;
             btnList = getBtnList()
             ctx.pantalla.beginPath()
             btnList.forEach(e => ctx.pantalla.drawImage( e.img, e.x, e.y, e.w, e.h));
             ctx.pantalla.closePath()
         },
 
-        playersMapa (p) {
-            console.log(p.bloque)
+        playersMapa: function (p) {
+            //console.log(p.bloque)
+            $DOM.e_mapa.width = $DOM.e_mapa.width 
             ctx.mapa.beginPath();
             ctx.mapa.lineWidth = 1;
             ctx.mapa.strokeStyle = "#ffffff";
@@ -517,7 +577,7 @@
             ctx.mapa.closePath();
         },
 
-        players () {
+        players: function () {
             ctx.juego.lineWidth = h * 0.009;
             ctx.juego.strokeStyle = "#ffffff";
             ctx.juego.font = h * 0.05 + "px Chunk";
@@ -547,8 +607,8 @@
             });
         },
 
-        bloquesMapa () {
-            $this.fondo.width = $this.fondo.width
+        bloquesMapa: function () {
+            $DOM.e_fondo.width = $DOM.e_fondo.width
             ctx.fondo.beginPath();
             blockList.forEach( e => {
                 console.log(e.c.length)
@@ -561,7 +621,7 @@
             ctx.fondo.closePath();
         },
 
-        bloques () {
+        bloques: function () {
             ctx.juego.beginPath()
             ctx.juego.lineWidth = h * 0.02;
             ctx.juego.strokeStyle = "#42dfb0";
@@ -581,10 +641,11 @@
             ctx.juego.closePath();
         },
 
-        juego: function() {
-            $this.canvas.width = $this.canvas.width
+        juego: function () {
+            $DOM.e_canvas.width = $DOM.e_canvas.width
             this.bloques()
             this.players()
+            
         }
     }
 
@@ -642,10 +703,21 @@
 
     const ball = {
 
+        main: function(){
+            fps.run()
+            window.requestAnimationFrame(ball.main);
+        },
+
+        run: function(){
+            if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( navigator.userAgent))
+                controller.useTouch()
+            else 
+                controller.useKeyboard()
+        }
     }
 
-    $this.play.onclick = (function () {
-        game.start();
+    $DOM.e_play.onclick = (function () {
+        ball.run();
     });
 
 //})();
